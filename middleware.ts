b869e6d77febe534683bd1fa4157e342f23786ed
Jimmy Heller <ali.tommy.heller2009@gmail.com>
2024-10-27 18:10:50 +0100

    extending the hasVisited cookie

--- middleware.ts
+++ middleware.ts
@@ -8,7 +8,7 @@ export function middleware(request: NextRequest) {
   // If it's the first visit and not already on the telegram-check page, redirect to telegram-check
   if (!hasVisited && pathname !== '/telegram-check') {
     const response = NextResponse.redirect(new URL('/telegram-check', request.url));
-    response.cookies.set('hasVisited', 'true', { maxAge: 60 * 60 * 24 }); // 1 day
+    response.cookies.set('hasVisited', 'true', { maxAge: 60 * 60 * 24 * 30 }); // 30 days
     return response;
   }
 
