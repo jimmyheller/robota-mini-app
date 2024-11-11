8ddb264d300a32cc6a0dbfa80518e1089647d7a0
Jimmy Heller <ali.tommy.heller2009@gmail.com>
2024-11-11 21:50:26 +0100

    - adding custom green - change community join to use the button component - change progress bar to have animation - change token logo with new logo svg - change telegramCheckClient to apply the designs

--- app/components/CommunityJoin.tsx
+++ app/components/CommunityJoin.tsx
@@ -1,13 +1,12 @@
 // components/CommunityJoin.tsx
 import React from 'react';
+import Button from './Button';
 
 const CommunityJoin: React.FC = () => (
   <div className="bg-gray-800 rounded-lg p-4 mb-4">
-    <h2 className="text-white font-bold mb-2">ROBOTA COMMUNITY</h2>
+    <h2 className="text-white font-bold mb-2">TODO COMMUNITY</h2>
     <p className="text-gray-300 mb-4">Join our Telegram for the latest and greatest in $TODO fun!</p>
-    <button className="w-full bg-white text-black py-2 rounded-lg font-semibold">
-      Join
-    </button>
+    <Button text="Continue..." href="/home" />
   </div>
 );
 
