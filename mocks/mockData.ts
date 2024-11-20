c8e08c080c3ecfc382c288ded120e5fd141762d6
jimmyheller <ali.tommy.heller2009@gmail.com>
2024-11-20 23:23:29 +0100

    deployment/nov20 (#11)
    * fixing the total friends number
    
    * fixing the url of the friend url
    
    * cleaning up the code
    
    * updated the libraries to have the mock data
    
    * adding local environment
    
    * used toLocaleString() funtcion to show the numbers in correct format
    
    * optimized imports

---
+++ mocks/mockData.ts
@@ -0,0 +1,31 @@
+export const MOCK_TELEGRAM_DATA = {
+    "initData": "user=%7B%22id%22%3A88206168%2C%22first_name%22%3A%22Jimmy%22%2C%22last_name%22%3A%22Heller%22%2C%22username%22%3A%22jimmyheller%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=4508367338390969759&chat_type=private&auth_date=1727639162&hash=e005e7a9260cd8f1da8a372b264e6835d3f602dcc50001a444472e274b73d3a4",
+    "colorScheme": "dark",
+    "themeParams": {
+        "section_header_text_color": "#b1c3d5",
+        "accent_text_color": "#2ea6ff",
+        "button_text_color": "#ffffff",
+        "destructive_text_color": "#ef5b5b",
+        "hint_color": "#b1c3d5",
+        "bottom_bar_bg_color": "#213040",
+        "text_color": "#ffffff",
+        "button_color": "#2ea6ff",
+        "secondary_bg_color": "#131415",
+        "subtitle_text_color": "#b1c3d5",
+        "bg_color": "#18222d",
+        "header_bg_color": "#131415",
+        "link_color": "#62bcf9",
+        "section_bg_color": "#18222d"
+    },
+    "isExpanded": true,
+    "viewportHeight": 645,
+    "viewportStableHeight": 645,
+    "headerColor": "#18222d",
+    "backgroundColor": "#18222d",
+    "isClosingConfirmationEnabled": false,
+    "version": "7.10"
+}
+
+export const MOCK_TELEGRAM_USER = {
+    telegramId: "88206168"
+}
